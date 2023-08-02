d5b73c0fce3e0c44468c319d12985a5881e40b82
Eduardo Nishimura <edunish@gmail.com>
2023-08-02 02:11:05 -0300

    Função de loop que para a animação quando o dinossauro colide com o cacto

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-var dino = document.querySelector(".dino");
+const dino = document.querySelector('.dino');
+const cacto = document.querySelector('.cactus');
 
 const jump = () => {
     console.log(dino);
@@ -6,7 +7,21 @@ const jump = () => {
 
     setTimeout(()=>{
         dino.classList.remove('jump');
-    },500);
+    },500); 
 }
+
+const loop = setInterval(() => {
+    
+    const cactoPosition = cacto.offsetLeft;
+    const dinoPosition = +window.getComputedStyle(dino).bottom.replace('px', '');
+    
+    console.log(dinoPosition);
+
+    if(cactoPosition <= 170 && cactoPosition > 0  && dinoPosition < 60){
+        cacto.style.animation = 'none';
+        cacto.style.left = `${cactoPosition}px`;
+    }
+
+}, 10);
 
 document.addEventListener('keydown', jump); 
